f18ddbfe3cb19978948b31008cddab098893f858
sharathon <138051877+sharathon@users.noreply.github.com>
2023-06-28 22:28:17 +0100

    hi

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,7 @@ export default class MyDocument extends Document {
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
         </Head>
         <body>
+          <p>hi</p>
           <Main />
           <NextScript />
         </body>
